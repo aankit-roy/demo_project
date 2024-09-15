79d9b7135f44584c74ffb31d62543dc91dd31dd1
Ankit kumar Roy <101168375+aankit-roy@users.noreply.github.com>
2024-09-15 16:14:05 +0530

    fourth commit

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,5 +1,4 @@
 import 'package:demo_project/constants/app_color.dart';
-import 'package:demo_project/main.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:demo_project/screens/home_screen.dart';
